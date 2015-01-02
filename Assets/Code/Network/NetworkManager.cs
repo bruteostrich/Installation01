c4d2487cb41372bbaacbdf3359dd1be3f35a1043
Chris Nobrega <chris.1068@outlook.com>
2015-01-01 20:21:53 -0500

    Added Network Manager, Connection State UI Text

---
+++ Assets/Code/Network/NetworkManager.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using ExitGames.Client.Photon;
+
+public class NetworkManager : Photon.MonoBehaviour 
+{
+    public string version = "installation 01"; // string used by photon to seperate different builds of the game, so that old builds wont work with new ones.
+    public Text connectionStateText;
+
+    private void Awake ()
+    {
+        DontDestroyOnLoad (this.gameObject);
+    }
+
+    private void Update ()
+    {
+        if (this.connectionStateText)
+            this.connectionStateText.text = PhotonNetwork.connectionStateDetailed.ToString ();
+    }
+
+    /// <summary>
+    /// Established initial connection to the photon server, returns if we are already connected to the server.
+    /// </summary>
+    public void ConnectToServer ()
+    {
+        if (PhotonNetwork.connected)
+            return;
+
+        PhotonNetwork.offlineMode = false;
+        PhotonNetwork.autoCleanUpPlayerObjects = true;
+        PhotonNetwork.automaticallySyncScene = true;
+        PhotonNetwork.autoJoinLobby = true;
+
+        PhotonNetwork.ConnectToBestCloudServer (this.version);
+    }
+
+    /// <summary>
+    /// Disconnects from the photon server, returns if we are not connected to the photon server.
+    /// </summary>
+    public static void DisconnectFromServer ()
+    {
+        if (!PhotonNetwork.connected)
+            return;
+
+        PhotonNetwork.LeaveLobby ();
+    }
+
+    /// <summary>
+    /// Join room on the photon server.
+    /// </summary>
+    /// <param name="room"></param>
+	public static void JoinRoom (string room)
+    {
+        PhotonNetwork.JoinRoom (room);
+    }
+
+    /// <summary>
+    /// Joins first available room on the photon network.
+    /// </summary>
+    public static void JoinRandomRoom ()
+    {
+        PhotonNetwork.JoinRandomRoom ();
+    }
+
+    /// <summary>
+    /// Leave the current room, returns if we are not in a room.
+    /// </summary>
+    public static void LeaveRoom ()
+    {
+        if (!PhotonNetwork.inRoom)
+            return;
+
+        PhotonNetwork.LeaveRoom ();
+    }
+
+    /// <summary>
+    /// Creates a new room on the photon server.
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="maxplayers"></param>
+    /// <param name="open"></param>
+    /// <param name="visible"></param>
+    public static void CreateRoom (string name, int maxplayers, bool open, bool visible)
+    {
+        RoomOptions options = new RoomOptions() { maxPlayers = maxplayers, isOpen = open, isVisible = visible };
+        PhotonNetwork.CreateRoom (name, options, TypedLobby.Default);
+    }
+}
