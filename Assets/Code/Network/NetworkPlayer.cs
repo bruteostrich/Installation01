b95dc513eba0a5591e9b6e9a6ccae571a37b8bdf
Chris Nobrega <chris.1068@outlook.com>
2015-01-02 20:11:34 -0500

    Player Position Now Serialized Not Observed

--- Assets/Code/Network/NetworkPlayer.cs
+++ Assets/Code/Network/NetworkPlayer.cs
@@ -14,41 +14,27 @@ public class NetworkPlayer : Photon.MonoBehaviour
         {
             foreach (Behaviour childCompnent in this.gameObject.GetComponentsInChildren<Behaviour>())
                 childCompnent.enabled = true;
-
-            /*this.gameObject.GetComponentsInChildren(System.Type ty)
-            this.GetComponent<CharacterController>().enabled = true;
-            this.GetComponent<vp_FPController>().enabled = true;
-            this.GetComponent<vp_FPInput>().enabled = true;
-            this.GetComponent<vp_FPPlayerEventHandler>().enabled = true;
-            this.GetComponent<vp_FPWeaponHandler>().enabled = true;
-            this.GetComponent<vp_ItemIdentifier>().enabled = true;
-
-            GameObject FPSCamera = this.gameObject.transform.FindChild("FPSCamera").gameObject;
-            FPSCamera.SetActive(true);
-            FPSCamera.GetComponent<AudioListener>().enabled = true;
-            FPSCamera.GetComponent<Camera>().enabled = true;
-            FPSCamera.GetComponent<vp_FPCamera>().enabled = true;
-
-            GameObject WeaponCamera = FPSCamera.gameObject.transform.FindChild("WeaponCamera").gameObject;
-            WeaponCamera.SetActive(true);
-            WeaponCamera.GetComponent<Camera>().enabled = true;
-
-            GameObject AR = FPSCamera.gameObject.transform.FindChild("AR").gameObject;
-            AR.SetActive(true);
-            AR.GetComponent<AudioSource>().enabled = true;
-            AR.GetComponent<vp_FPWeapon>().enabled = true;
-            AR.GetComponent<vp_FPWeaponShooter>().enabled = true;
-            AR.GetComponent<vp_FPWeaponReloader>().enabled = true;
-            AR.GetComponent<vp_ItemIdentifier>().enabled = true;
-
-            GameObject MuzzleFlash = FPSCamera.gameObject.transform.FindChild("ARMuzzleFlash").gameObject;
-            MuzzleFlash.SetActive(true);
-            MuzzleFlash.GetComponent<vp_MuzzleFlash>().enabled = true;*/
         }
         else
         {
             foreach (Behaviour childCompnent in this.gameObject.GetComponentsInChildren<Behaviour>())
                 childCompnent.enabled = false;
+        }
+    }
+
+    private void OnPhotonSerializeView (PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            // local player, send our position info
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+        }
+        else
+        {
+            // remote player, receive position info
+            this.transform.position = (Vector3)stream.ReceiveNext();
+            this.transform.rotation = (Quaternion)stream.ReceiveNext();
         }
     }
 }
