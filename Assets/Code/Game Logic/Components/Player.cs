6005cf97d8080af86140ca1c88285cac531d6766
HaMM4R <HaMM4RTime@hotmail.com>
2015-01-07 10:44:44 +0000

    Update 3
    Once again if any issues with errors just remove the changes but ill sort this out properly when I get home.

--- Assets/Code/Game Logic/Components/Player.cs
+++ Assets/Code/Game Logic/Components/Player.cs
@@ -41,13 +41,26 @@ namespace GameLogic
         {
             if(Input.GetAxis("Horizontal") && charMotor.isGrounded || Input.GetAxis("Vertical") && charMotor.isGrounded)
             {
-                animationHolder.animation.Crossfade("Walk");
-                charMotor.movement.maxForwardSpeed
-                charMotor.movement.maxForwardSpeed
-                charMotor.movement.maxForwardSpeed
+                if (Input.GetKey(KeyCode.LeftShift))
+                {
+                    //Controls walking speed/animation
+                    animationHolder.animation.Crossfade("Run");
+                    charMotor.movement.maxForwardSpeed = playerStats.walkSpeed;
+                    charMotor.movement.maxBackwardsSpeed = playerStats.walkSpeed - 1;
+                    charMotor.movement.maxSidewaysSpeed = playerStats.walkSpeed;
+                }
+                else
+                {
+                    //Controls running speed/animation
+                    animationHolder.animation.Crossfade("Walk");
+                    charMotor.movement.maxForwardSpeed = playerStats.runSpeed;
+                    charMotor.movement.maxBackwardsSpeed = playerStats.runSpeed - 1;
+                    charMotor.movement.maxSidewaysSpeed = playerStats.runSpeed;
+                }
             }
             else
             {
+                //Plays the idle animation when just standing
                 animationHolder.animation.Crossfade("Idle");
             }
         }
@@ -67,14 +80,21 @@ namespace GameLogic
     [System.Serializable]
     public class PlayerStats
     {
+        //Information for player physical stats
         public float health;
         public float shields;
         public float stamina;
         
+        //Score stuff, needs to be synced with the networkmanager
         public int kills;
         public int deaths;
         public int scores;
         
+        //Speed value holders
+        public int walkSpeed;
+        public int runSpeed;
+        
+        //Helpful bool for displaying GUI when dead. Set to false when dead and true when alive
         public bool isAlive;
     }
 }
