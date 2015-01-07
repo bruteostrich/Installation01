708b4ca4c385587759bcad956ddf4f2784fb9102
HaMM4R <HaMM4RTime@hotmail.com>
2015-01-07 11:15:44 +0000

    Update 4 (to player)
    As I say, if it throws any errors just delete the lines :)

--- Assets/Code/Game Logic/Components/Player.cs
+++ Assets/Code/Game Logic/Components/Player.cs
@@ -14,11 +14,14 @@ namespace GameLogic
         private GameManager Manager;
         public PlayerStats playerStats;
         
+        //Vars for controlling the playerSpeed/animationSpeed
         public CharacterController charCont;
         public CharacterMotor charMotor; 
         
+        //Layer to hold the animations for the walking/running/idle
         public Transform animationHolder;
         
+        //Var that takes the speed of the player so I can control the player animation speed based upon that of the player.
         public float velocityMag;
 
         void Start()
@@ -33,6 +36,7 @@ namespace GameLogic
         
         void FixedUpdate()
         {
+            //Setting the velocity magnitude to that of the character controller
             velocityMag = charCont.velocity.magnitude;
             AnimSpeedController();
         }
@@ -41,9 +45,10 @@ namespace GameLogic
         {
             if(Input.GetAxis("Horizontal") && charMotor.isGrounded || Input.GetAxis("Vertical") && charMotor.isGrounded)
             {
-                if (Input.GetKey(KeyCode.LeftShift))
+                if (Input.GetKey(KeyCode.LeftShift) && charMotor.isGrounded)
                 {
                     //Controls walking speed/animation
+                    animationHolder.animation["Run"].speed = velocityMag / playerStats.runSpeed * 1.2f;
                     animationHolder.animation.Crossfade("Run");
                     charMotor.movement.maxForwardSpeed = playerStats.walkSpeed;
                     charMotor.movement.maxBackwardsSpeed = playerStats.walkSpeed - 1;
@@ -52,6 +57,7 @@ namespace GameLogic
                 else
                 {
                     //Controls running speed/animation
+                    animationHolder.animation["Walk"].speed = velocityMag / playerStats.walkSpeed;
                     animationHolder.animation.Crossfade("Walk");
                     charMotor.movement.maxForwardSpeed = playerStats.runSpeed;
                     charMotor.movement.maxBackwardsSpeed = playerStats.runSpeed - 1;
@@ -63,6 +69,34 @@ namespace GameLogic
                 //Plays the idle animation when just standing
                 animationHolder.animation.Crossfade("Idle");
             }
+        }
+        
+        public void Spawn()
+        {
+            playerStats.health = 20; 
+            playerStats.shields = 100; 
+        }
+        
+        [RPC]
+        public void Server_TakeDamage(float damage)
+        {
+            Client_TakeDamage(damage)
+        }
+        
+        void Client_TakeDamage(float damage)
+        {
+            if(playerStats.shields > 0)
+                playerStats.shields -= damage;
+            else
+                playerStats.health -= damage; 
+            
+            if(playerStats.health <= 0)
+                Die();
+        }
+        
+        void Die()
+        {
+            
         }
 
         private void InitializeInternals()
